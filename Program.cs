c0c42c51fe8538e132ec99daa42a198f51eada6f
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 20:31:28 +0530

    [Abhinav]-adding pop and peek part

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿using Stack;
 
 Stack1 stack1 = new Stack1();
+
 stack1.Push(70);
 stack1.Push(30);
 stack1.Push(56);
+stack1.Display();
+Console.WriteLine("\n\n");
+//stack1.Pop();
+//stack1.Pop();
+//stack1.Pop();
+//stack1.Display();
+stack1.Peek();
 stack1.Display();
