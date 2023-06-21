848692ead261ad5bf436e7cbd7b9db4f27bcfd6b
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 20:18:02 +0530

    [Abhinav]-adding insertion part

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Stack;
+
+Stack1 stack1 = new Stack1();
+stack1.Push(70);
+stack1.Push(30);
+stack1.Push(56);
+stack1.Display();
---
+++ Stack1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack1
+    {
+        Node top;
+        public Stack1()
+        {
+            top = null;
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.WriteLine("|" + temp.Data + "|");
+                    Console.WriteLine("__________");
+                    temp = temp.Next;
+                }
+            }
+        }
+        public void Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                top = top.Next;
+            }
+        }
+    }
+}
